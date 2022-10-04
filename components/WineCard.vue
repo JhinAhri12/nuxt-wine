57615197ce1aa5d64a815fb7875821b0c09b58ee
JhinAhri12 <cyprien.rezki@hotmail.fr>
2022-10-04 18:08:32 +0200

    add details wine data

--- components/WineCard.vue
+++ components/WineCard.vue
@@ -3,30 +3,41 @@
         <div class="card m-3">
             <div class="row">
                 <div class="col-sm-8">
-                    <img src="../assets/banner.jpg" class="img-thumbnail m-2" alt="">
+                    <img :src="wine.img" class="img-thumbnail m-2" alt="">
                 </div>
                     <div class="col-sm-2">
                         <div class="card-body">
                             <p class="text-muted">Type</p>
-                            <p>{{wine.</p>
+                            <p>{{wine.type}}</p>
                             <p class="text-muted">Years</p>
+                            <p>{{wine.years}}</p>
                             <p class="text-muted">Origin</p>
+                            <p>{{wine.origin}}</p>
                         </div>
                     </div>
                     <div class="col-sm-2">
                         <div class="card-body">
                             <p class="text-muted">Capacity</p>
+                            <p>{{wine.capacity}}</p>
                             <p class="text-muted">Alcohol</p>
+                            <p>{{wine.alcohol}}</p>
                         </div>
                     </div>
             </div>
-            <h2>Nom</h2>
-            <h3>Domaine</h3>
-            <p><strong>Price</strong>&nbsp;<span>Contact</span>&nbsp;<span>Share</span></p>
+            <div class="text-center">
+
+            
+            <h2>{{wine.name}}</h2>
+            <h3>{{wine.domaine}}</h3>
+            <p><strong>Price</strong> {{wine.price}}&nbsp;<span><btn class="btn btn-wine">Contact</btn>&nbsp;<btn class="btn btn-wine">Partager</btn></span></p>
             <hr>
-            <p><span class="text-muted">Sale</span> <span class="text-muted">Reserve</span> <span class="text-muted">Arrived within</span></p>
+            <p><span class="text-muted">Sale</span> {{wine.sale}} <span class="text-muted">Reserve</span> {{wine.reserve}} <span class="text-muted">Arrived within</span> {{wine.date}}</p>
             <hr>
-            <button>Buy Now</button>
+            </div>
+            <div class="container text-center mb-3">
+                <btn class="btn btn-wine">Acheter</btn>
+            </div>
+            
         </div>
         
         
--- pages/wine/_id.vue
+++ pages/wine/_id.vue
@@ -1,7 +1,27 @@
 <template>
     <div class="wine-details">
         <Banner Page="Details" />
-        <WineCard />
+        <WineCard :wine="wine" />
     </div>
     
 </template>
+
+<script>
+export default {
+      async asyncData({ $axios, error, params }) {
+        try {
+          const { data } = await $axios.get(
+            'https://my-json-server.typicode.com/JhinAhri12/DBWine/wines/' + params.id
+          )
+          return {
+            wine: data
+          }
+        } catch (e) {
+          error({
+            statusCode: 503,
+            message: 'Unable to fetch wine #' + params.id
+          })
+        }
+      }
+}
+</script>
